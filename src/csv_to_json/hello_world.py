0751d514290458a88aa46c3a20d42d710c922eb0
Paul Ndambo <paulkadabo@gmail.com>
2023-05-05 22:47:05 +0300

    local changes added

---
+++ src/csv_to_json/hello_world.py
@@ -0,0 +1,6 @@
+def hello_world(name: str):
+    """
+    Takes in a string,
+    : returns a string
+    """
+    return f"Hello {name}"
---
+++ test_csv_to_json.py
@@ -0,0 +1,4 @@
+from csv_to_json import csv_to_json
+
+x = csv_to_json("students.csv")
+print(x)
